80ce20db876c6f9c263ae827fc2aa18f305a6f81
seabbs <s.e.abbott12@gmail.com>
2020-10-11 13:27:34 +0000

    simple delay updates

--- rt-forecast/functions/format-forecast.R
+++ rt-forecast/functions/format-forecast.R
@@ -4,12 +4,10 @@ require(data.table)
 require(lubridate)
 source(here::here("rt-forecast/functions/dates-to-epiweek.R"))
 
-
-# forecast_date -> date forecast was made
 format_forecast<- function(forecasts, 
-                           shrink_per = 0,
                            forecast_date = NULL, 
                            submission_date = NULL,
+                           CrI_samples = 1,
                            target = NULL) {
   
   # Filter to full epiweeks
@@ -21,25 +19,24 @@ format_forecast<- function(forecasts,
   weekly_forecasts_inc <- forecasts[,.(value = sum(value, na.rm = TRUE), target_end_date = max(date)), 
                                     by = .(epiweek, region, sample)]
   
+  shrink_per <- (1 - CrI_samples) / 2
   weekly_forecasts_inc <- weekly_forecasts_inc[order(value)][,
                                                .SD[round(.N * shrink_per, 0):round(.N * (1 - shrink_per), 0)],
                                                by = .(epiweek, region)]
   
   # Take quantiles
   weekly_forecasts <- weekly_forecasts_inc[, 
-                                           .(value = quantile(deaths, probs = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99), na.rm=T),
+                                           .(value = quantile(value, probs = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99), na.rm=T),
                                              quantile = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99), 
                                              target_end_date = max(target_end_date), target_value = "inc"), 
                                               by = .(region, epiweek)][order(region, epiweek)]
   
-  
-
   # Add necessary columns
-  # dates and types
   forecasts_format <- weekly_forecasts[, `:=`(forecast_date = forecast_date,
                                               submission_date = submission_date,
                                               type = "quantile",
-                                              location = region,
+                                              location_name = region,
+                                              location = ifelse(region == "Germany", "GM", "PL"),
                                               horizon = 1 + epiweek - lubridate::epiweek(submission_date))][,
                                    target := paste0(horizon, " wk ahead ", target_value, " ", target)]
   
@@ -53,9 +50,9 @@ format_forecast<- function(forecasts,
   
   # Set column order
   forecasts_format <- data.table::setcolorder(forecasts_format,
-                                              c("forecast_date", "submission_date", 
-                                                "target", "target_end_date", "location", 
-                                                "type", "quantile", "value"))
+                                              c("location", "location_name", "type", 
+                                                "quantile", "horizon", "value", "target_end_date",
+                                                "forecast_date", "target"))
   
   forecasts_format <- forecasts_format[target_end_date > forecast_date]
   return(forecasts_format)
---
+++ rt-forecast/update-submission.R
@@ -0,0 +1,43 @@
+# Package -----------------------------------------------------------------
+library(data.table)
+library(EpiNow2)
+
+# Dates -------------------------------------------------------------------
+target_date <- Sys.date()
+
+# Get forecasts -----------------------------------------------------------
+case_forecast <- suppressWarnings(
+  EpiNow2::get_regional_results(results_dir = here::here("rt-forecast", "samples", "cases"),
+                                date = lubridate::ymd(target_date),
+                                forecast = TRUE, samples = TRUE)$estimated_reported_cases$samples)
+
+death_forcast <- suppressWarnings(
+  EpiNow2::get_regional_results(results_dir = here::here("rt-forecast", "samples", "deaths"),
+                                date = lubridate::ymd(target_date),
+                                forecast = TRUE, samples = TRUE)$estimated_reported_cases$samples)
+
+# Format forecasts --------------------------------------------------------
+source(here::here("rt-forecast", "functions", "format-forecast.R"))
+
+case_forecast <- format_forecast(case_forecast, 
+                                 forecast_date = target_date,
+                                 submission_date = target_date,
+                                 CrI_samples = 0.4,
+                                 target = "cases")
+
+death_forecast <- format_forecast(death_forecast, 
+                                  forecast_date = target_date,
+                                  submission_date = target_date,
+                                  CrI_samples = 0.4,
+                                  target = "deaths")
+
+# Save forecasts ----------------------------------------------------------
+target_folder <- here::here("rt-forecast", "rt-forecast", "submissions", target_date)
+if (!dir.exists(target_folder)) {
+  dir.create(target_folder, recursive = TRUE)
+}
+
+data.table::fwrite(case_forecast[location_name == "Germany"], file.path(target_folder, "cases-germany.csv"))
+data.table::fwrite(case_forecast[location_name == "Poland"], file.path(target_folder, "cases-poland.csv"))
+data.table::fwrite(death_forecast[location_name == "Germany"], file.path(target_folder, "deaths-germany.csv"))
+data.table::fwrite(death_forecast[location_name == "Germany"], file.path(target_folder, "deaths-germany.csv"))
