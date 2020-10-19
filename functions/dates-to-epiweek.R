af968dee94888a68593d724db2f69853c838adc1
nikosbosse <nikosbosse@gmail.com>
2020-10-19 12:02:46 +0200

    update data and add affiliation field in app

--- functions/dates-to-epiweek.R
+++ functions/dates-to-epiweek.R
@@ -4,8 +4,8 @@ library(dplyr)
 dates_to_epiweek <- function(df){
   
   seq <- tibble::tibble(date = unique(df$date),
-                        epiweek = lubridate::epiweek(date),
-                        day = weekdays(date))
+                        epiweek = lubridate::epiweek(as.Date(date)),
+                        day = weekdays(as.Date(date)))
   
   epiweek_end_date <- seq %>%
     dplyr::filter(day == "Saturday")
@@ -17,7 +17,7 @@ dates_to_epiweek <- function(df){
     dplyr::left_join(epiweek_end_date, by = "epiweek")
   
   df_dated <- df %>%
-    dplyr::mutate(epiweek = lubridate::epiweek(date),
+    dplyr::mutate(epiweek = lubridate::epiweek(as.Date(date)),
                   epiweek_end = date %in% epiweek_end_date$date,
                   epiweek_full = epiweek %in% epiweek_complete$epiweek)
   
