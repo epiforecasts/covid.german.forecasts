1b0f6732e8961cdd7dc98048c092f9176e46190a
seabbs <s.e.abbott12@gmail.com>
2020-11-16 21:03:20 +0000

    add EpiNow2 forecast

--- rt-forecast/update-death.R
+++ rt-forecast/update-death.R
@@ -20,6 +20,9 @@ deaths <- deaths[, .(region = as.character(location_name), date = as.Date(date),
 deaths <- deaths[date >= (max(date) - lubridate::weeks(12))]
 data.table::setorder(deaths, region, date)
 
+# Set up parallel execution -----------------------------------------------
+setup_future(deaths, min_cores_per_worker = 2)
+
 # Run Rt estimation -------------------------------------------------------
 # default Rt settings
 rt <- opts_list(rt_opts(prior = list(mean = 1.1, sd = 0.2), 
@@ -31,7 +34,8 @@ regional_epinow(reported_cases = deaths,
                 generation_time = generation_time, 
                 delays = delay_opts(incubation_period, onset_to_death),
                 rt = rt,
-                stan = stan_opts(samples = 2000, warmup = 500, cores = 4), 
+                stan = stan_opts(samples = 2000, warmup = 250, chains = 2,
+                                 future = TRUE, max_execution_time = 45*60), 
                 horizon = 30,
                 output = c("region", "summary", "timing", "samples"),
                 target_date = target_date,
