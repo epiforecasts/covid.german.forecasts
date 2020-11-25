82fc1a253de8dab4ce846b3e4d2742c9806e8faa
nikosbosse <nikosbosse@gmail.com>
2020-11-26 00:09:55 +0100

    make evaluation ready for github pages

--- evaluation/forecast-evaluation.R
+++ evaluation/forecast-evaluation.R
@@ -19,18 +19,32 @@ names(file_paths) <- c("case", "death")
 truth_data <- purrr::map_dfr(file_paths, readr::read_csv, .id = "target_type") %>%
   dplyr::rename(true_value = value)
 
+intro_text <- c("This our own evaluation of individual forecasts submitted to the 
+EpiExpert model. This model is submitted to the 
+[German and Polish Forecast Hub](https://github.com/KITmetricslab/covid19-forecast-hub-de)
+each week. \n\n
+These evaluations are preliminary - this means we cannot rule out any mistakes 
+and the plots and analyses are subject to change. 
+The evaluations are also not authorised by the German Forecast Hub team.
+
+If you have questions or want to give feedback, please create an issue on our 
+[github repository](https://github.com/epiforecasts/covid-german-forecasts).")
+
 
 params <- list(locations = c("Germany", "Poland"),
                forecast_dates = "all",
                horizons = c(1:4),
-               target_types = "all")
+               target_types = "all",
+               intro_text = intro_text)
 
 
 
 scoringutils::render_scoring_report(truth_data = truth_data, 
+                                    document_title = "EpiExpert Crowd-Forecasting Performance Board",
                                     params = params,
                                     prediction_data = prediction_data, 
-                                    save_dir = "evaluation/")
+                                    save_dir = "docs/", 
+                                    filename = "index.html")
 
 
 
