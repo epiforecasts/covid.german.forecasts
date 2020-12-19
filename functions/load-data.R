a3fe76bd27892741113ce68ffba918d550e69d91
nikosbosse <nikosbosse@gmail.com>
2020-12-19 12:09:54 +0100

    separate download data from load data function

--- functions/load-data.R
+++ functions/load-data.R
@@ -1,5 +1,51 @@
 source(here::here("functions", "dates-to-epiweek.R"))
 
+download_data <- function(country = "Germany_Poland", 
+                          save_dir = "data") {
+  # country is US
+  if (country == "US") {
+    incident_cases <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Incident%20Cases.csv?raw=true")
+    incident_deaths <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Incident%20Deaths.csv?raw=true")
+    
+    if (!weekly) {
+      # we only need cumulative data if we don't sum up anyway. Could in principle also drop cumulative data entirely
+      cumulative_cases <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Cumulative%20Cases.csv?raw=true")
+      cumulative_deaths <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Cumulative%20Deaths.csv?raw=true")
+    }
+    
+    # country not US
+  } else if (country == "Germany_Poland") {
+    incident_cases <- data.table::rbindlist(list(
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Incident%20Cases_Germany.csv"), 
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Incident%20Cases_Poland.csv")
+    ))
+    cumulative_cases <- data.table::rbindlist(list(
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Cumulative%20Cases_Germany.csv"), 
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Cumulative%20Cases_Poland.csv")
+    ))
+    incident_deaths <- data.table::rbindlist(list(
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Incident%20Deaths_Germany.csv"), 
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Incident%20Deaths_Poland.csv")
+    ))
+    cumulative_deaths <- data.table::rbindlist(list(
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Cumulative%20Deaths_Germany.csv"), 
+      readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Cumulative%20Deaths_Poland.csv")
+    ))
+  }
+  
+  # write incident cases and deaths
+  data.table::fwrite(incident_cases, here::here(save_dir, paste0("daily-incidence-cases-", country, ".csv")))
+  data.table::fwrite(incident_deaths, here::here(save_dir, paste0("daily-incidence-deaths-", country, ".csv")))
+  
+  data.table::fwrite(cumulative_cases, here::here(save_dir, paste0("daily-cumulative-cases-", country, ".csv")))
+  data.table::fwrite(cumulative_deaths, here::here(save_dir, paste0("daily-cumulative-deaths-", country, ".csv")))
+}
+
+
+
+
+
+
 get_data <- function(load_from_server = FALSE, 
                      cumulative = FALSE,
                      cases = TRUE,
@@ -7,47 +53,7 @@ get_data <- function(load_from_server = FALSE,
                      weekly = TRUE) {
   
   if (load_from_server) {
-    
-    # country is US
-    if (country == "US") {
-      incident_cases <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Incident%20Cases.csv?raw=true")
-      incident_deaths <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Incident%20Deaths.csv?raw=true")
-      
-      if (!weekly) {
-        # we only need cumulative data if we don't sum up anyway. Could in principle also drop cumulative data entirely
-        cumulative_cases <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Cumulative%20Cases.csv?raw=true")
-        cumulative_deaths <- readr::read_csv("https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Cumulative%20Deaths.csv?raw=true")
-      }
-    
-    # country not US
-    } else if (country == "Germany_Poland") {
-      incident_cases <- data.table::rbindlist(list(
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Incident%20Cases_Germany.csv"), 
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Incident%20Cases_Poland.csv")
-      ))
-      cumulative_cases <- data.table::rbindlist(list(
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Cumulative%20Cases_Germany.csv"), 
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Cumulative%20Cases_Poland.csv")
-      ))
-      incident_deaths <- data.table::rbindlist(list(
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Incident%20Deaths_Germany.csv"), 
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Incident%20Deaths_Poland.csv")
-      ))
-      cumulative_deaths <- data.table::rbindlist(list(
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/RKI/truth_RKI-Cumulative%20Deaths_Germany.csv"), 
-        readr::read_csv("https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Cumulative%20Deaths_Poland.csv")
-      ))
-    }
-    
-    # write incident cases and deaths
-    data.table::fwrite(incident_cases, here::here("data", paste0("daily-incidence-cases-", country, ".csv")))
-    data.table::fwrite(incident_deaths, here::here("data", paste0("daily-incidence-deaths-", country, ".csv")))
-    
-    data.table::fwrite(cumulative_cases, here::here("data", paste0("daily-cumulative-cases-", country, ".csv")))
-    data.table::fwrite(cumulative_deaths, here::here("data", paste0("daily-cumulative-deaths-", country, ".csv")))
-
-  # ----------------------------------------------------------------------------
-  # if not load from server
+    download_data(country = country)
   } else {
     incident_cases <- data.table::fread(here::here("data", paste0("daily-incidence-cases-", country, ".csv")))
     incident_deaths <- data.table::fread(here::here("data", paste0("daily-incidence-deaths-", country, ".csv")))
