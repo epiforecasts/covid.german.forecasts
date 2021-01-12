ea9b3e2507608071fa53c539f48209020e5e101e
nikosbosse <nikosbosse@gmail.com>
2021-01-12 01:37:44 +0100

    implement functionality to obtain samples

--- update-scripts/human-rt-forecast.R
+++ update-scripts/human-rt-forecast.R
@@ -78,325 +78,96 @@ data.table::fwrite(raw_forecasts %>%
                           submission_date, "-raw-forecasts.csv"))
 
 
+# draw samples from the distributions ------------------------------------------
+n_people <- filtered_forecasts$forecaster_id %>%
+  unique() %>%
+  length()
+
+draw_samples <- function(distribution, 
+                         median, 
+                         width,
+                         n_people, 
+                         overall_sample_number = 1000, 
+                         min_per_person_samples = 50) {
+
+  num_samples <- max(ceiling(overall_sample_number / n_people), 
+                     min_per_person_samples)
+  
+  if (distribution == "log-normal") {
+    values <- exp(rnorm(num_samples, 
+                             mean = log(as.numeric(median)),
+                             sd = as.numeric(width)))
+  } else if (distribution == "normal") {
+    values <- rnorm(num_samples, 
+                          mean = (as.numeric(median)),
+                          sd = as.numeric(width))
+    
+  } else if (distribution == "cubic-normal") {
+    values <- (rnorm(num_samples, 
+                          mean = (as.numeric(median) ^ (1/3)),
+                          sd = as.numeric(width))) ^ 3
+    
+  } else if (distribution == "fifth-power-normal") {
+    values <- (rnorm(num_samples, 
+                          mean = (as.numeric(median) ^ (1/5)),
+                          sd = as.numeric(width))) ^ 5
+    
+  } else if (distribution == "seventh-power-normal") {
+    values <- (rnorm(num_samples, 
+                          mean = (as.numeric(median) ^ (1/7)),
+                          sd = as.numeric(width))) ^ 7
+  }
+
+  out <- list(sort(values))
+  return(out)
+}
+
+# draw samples
+forecast_samples <- filtered_forecasts %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(value = draw_samples(median = median, 
+                                     width = width, 
+                                     distribution = distribution,
+                                     n_people = n_people, 
+                                     overall_sample_number = 1000, 
+                                     min_per_person_samples = 50), 
+                sample = list(1:length(value))) %>%
+  tidyr::unnest(cols = c(sample, value)) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(type = "rt", 
+                target = paste0(horizon, " wk ahead inc ", type), 
+                type = "sample")
 
 
 
 
-
-# set a min sample per person and a min target samples. Set the number of 
-# samples per person to the taregt number / number of people
-# obtain samples for every weekly forecast
-# order samples
 # do linear interpolation between the samples for the week days
 # get the ordering for the samples from the last data point to stitch the 
 # last observation to the first forecast. 
 
+forecast_samples <- forecast_samples %>%
+  dplyr::mutate(horizon = ifelse(horizon == 1, 1, (horizon - 1) * 7)) %>%
+  pull(horizon) %>% unique()
+
+daily_set <- 0
 
 
-
-# Do EpiNow2 magic: 
-# combine EpiNow2 model per region with expert samples and use 
-# simulate_cases
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# obtain quantiles from forecasts ----------------------------------------------
-# define function that returns quantiles depending on condition and distribution
-calculate_quantiles <- function(quantile_grid, 
-                                median, 
-                                width,
-                                distribution) {
-  
-  if (distribution == "log-normal") {
-      values <- list(exp(qnorm(quantile_grid, 
-                               mean = log(as.numeric(median)),
-                               sd = as.numeric(width))))
-    } else if (distribution == "normal") {
-      values <- list((qnorm(quantile_grid, 
-                            mean = (as.numeric(median)),
-                            sd = as.numeric(width))))
-      
-    } else if (distribution == "cubic-normal") {
-      values <- list((qnorm(quantile_grid, 
-                            mean = (as.numeric(median) ^ (1/3)),
-                            sd = as.numeric(width))) ^ 3)
-      
-    } else if (distribution == "fifth-power-normal") {
-      values <- list((qnorm(quantile_grid, 
-                            mean = (as.numeric(median) ^ (1/5)),
-                            sd = as.numeric(width))) ^ 5)
-      
-    } else if (distribution == "seventh-power-normal") {
-      values <- list((qnorm(quantile_grid, 
-                            mean = (as.numeric(median) ^ (1/7)),
-                            sd = as.numeric(width))) ^ 7)
-    }
-  return(values)
-}
-
-forecast_quantiles <- filtered_forecasts %>%
-  # disregard quantile forecasts this week
-  dplyr::rowwise() %>%
-  dplyr::mutate(quantile = list(quantile_grid),
-                value = calculate_quantiles(quantile_grid, 
-                                            median, 
-                                            width, 
-                                            distribution)) %>%
-  tidyr::unnest(cols = c(quantile, value)) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(type = "rt", 
-                target = paste0(horizon, " wk ahead inc ", type), 
-                type = "quantile")
-
-
-## extrapolate quantiles in between
-d <- forecast_quantiles %>%
-  group_by(forecaster_id, region, quantile) %>%
-  dplyr::mutate(difference = c(diff(value), 0)) %>%
+# interpolate missing days
+# I'm pretty sure the horizon time indexisng is currently wrong. 
+forecast_samples <- forecast_samples %>%
   tidyr::uncount(7, .id = "counter") %>%
-  dplyr::mutate()
+  dplyr::mutate(horizon = counter + 7 * (horizon - 1)) %>%
+  dplyr::filter(horizon <= 36) %>%
+  dplyr::mutate(value = ifelse(horizon %in% c(1, 8, 15, 22, 29, 36), 
+                               value, NA)) %>%
+  dplyr::group_by(forecaster_id, region, sample) %>%
+  dplyr::mutate(value = zoo::na.approx(value))
+
 
 
 # save forecasts in quantile-format
-data.table::fwrite(forecast_quantiles %>%
+data.table::fwrite(forecast_samples %>%
                      dplyr::mutate(submission_date = submission_date),
                    paste0("rt-crowd-forecast/processed-forecast-data/", 
                           submission_date, "-processed-forecasts.csv"))
-
-# # filter out all forecasts where only one of two targets was submitted
-# forecast_quantiles %>%
-#   dplyr::group_by(forecaster_id) %>%
-#   dplyr::mutate(number_flag = dplyr::n()) %>%
-#   dplyr::filter(number_flag %% 8 == 0) # 4 * 4 * 23 in total
-
-
-
-if (median_ensemble) {
-  # make median ensemble ---------------------------------------------------------
-  median_ensemble <- forecast_quantiles %>%
-    dplyr::mutate(target_end_date = as.Date(target_end_date)) %>%
-    dplyr::group_by(location, location_name, target, type, quantile, horizon, target_end_date) %>%
-    dplyr::summarise(value = median(value)) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(target, target_end_date, location, type, quantile, value, location_name)
-  
-  # add median forecast
-  forecast_inc <- dplyr::bind_rows(median_ensemble, 
-                                   median_ensemble %>%
-                                     dplyr::filter(quantile == 0.5) %>%
-                                     dplyr::mutate(type = "point", 
-                                                   quantile = NA))
-} else {
-  # make mean ensemble ---------------------------------------------------------
-  mean_ensemble <- forecast_quantiles %>%
-    dplyr::mutate(target_end_date = as.Date(target_end_date)) %>%
-    dplyr::group_by(location, location_name, target, type, quantile, horizon, target_end_date) %>%
-    dplyr::summarise(value = mean(value)) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(target, target_end_date, location, type, quantile, value, location_name)
-  
-  # add median forecast
-  forecast_inc <- dplyr::bind_rows(mean_ensemble, 
-                                   mean_ensemble %>%
-                                     dplyr::filter(quantile == 0.5) %>%
-                                     dplyr::mutate(type = "point", 
-                                                   quantile = NA))
-}
-
-
-# add cumulative forecasts -----------------------------------------------------
-
-# get latest cumulative forecast
-first_forecast_date <- forecasts %>%
-  dplyr::pull(target_end_date) %>%
-  as.Date() %>%
-  unique() %>%
-  min(na.rm = TRUE)
-
-# add latest deaths and cases
-source(here::here("functions", "load-data.R"))
-
-deaths <- get_data(cumulative = TRUE, weekly = TRUE, cases = FALSE) %>%
-  dplyr::group_by(location) %>%
-  dplyr::filter(target_end_date == as.Date(first_forecast_date - 7)) %>%
-  dplyr::mutate(case = "death")
-
-cases <- get_data(cumulative = TRUE, weekly = TRUE, cases = TRUE) %>%
-  dplyr::group_by(location) %>%
-  dplyr::filter(target_end_date == as.Date(first_forecast_date - 7)) %>%
-  dplyr::mutate(case = "case")
-
-last_obs <- dplyr::bind_rows(deaths, cases) %>%
-  dplyr::select(location, value, case) %>%
-  dplyr::rename(last_value = value)
-
-# make cumulative
-forecast_cum <- forecast_inc %>%
-  dplyr::mutate(case = ifelse(grepl("case", target), "case", "death")) %>%
-  dplyr::group_by(location, quantile, case) %>%
-  dplyr::mutate(value = cumsum(value), 
-                target = gsub("inc", "cum", target)) %>%
-  dplyr::ungroup() %>%
-  # add last observed value
-  dplyr::inner_join(last_obs) %>%
-  dplyr::mutate(value = value + last_value) %>%
-  dplyr::select(-last_value, -case)
-
-forecast_submission <- dplyr::bind_rows(forecast_inc, forecast_cum) %>%
-  dplyr::mutate(forecast_date = submission_date)
-
-
-# write submission files -------------------------------------------------------
-if (!dir.exists(here::here("submissions", "human-forecasts", submission_date))) {
-  dir.create(here::here("submissions", "human-forecasts", submission_date))
-}
-
-
-
-forecast_submission %>%
-  dplyr::filter(location_name %in% "Germany", 
-                grepl("death", target)) %>%
-  data.table::fwrite(here::here("submissions", "human-forecasts", submission_date,
-                                paste0(submission_date, 
-                                       "-Germany-epiforecasts-EpiExpert.csv")))
-
-forecast_submission %>%
-  dplyr::filter(location_name %in% "Germany", 
-                grepl("case", target)) %>%
-  data.table::fwrite(here::here("submissions", "human-forecasts", submission_date,
-                                paste0(submission_date, 
-                                       "-Germany-epiforecasts-EpiExpert-case.csv")))
-
-forecast_submission %>%
-  dplyr::filter(location_name %in% "Poland", 
-                grepl("death", target)) %>%
-  data.table::fwrite(here::here("submissions", "human-forecasts", submission_date,
-                                paste0(submission_date, 
-                                       "-Poland-epiforecasts-EpiExpert.csv")))
-
-forecast_submission %>%
-  dplyr::filter(location_name %in% "Poland", 
-                grepl("case", target)) %>%
-  data.table::fwrite(here::here("submissions", "human-forecasts", submission_date,
-                                paste0(submission_date, 
-                                       "-Poland-epiforecasts-EpiExpert-case.csv")))
-
-
-
-
-
-
-
-
-# also read all EpiExpert forecasts, give them a board_name 
-folders <- list.files("submissions/human-forecasts/")
-files <- purrr::map(folders, 
-                    .f = function(folder_name) {
-                      files <- list.files(paste0("submissions/human-forecasts/", 
-                                                 folder_name))
-                      paste(paste0("submissions/human-forecasts/", 
-                                   folder_name, "/", 
-                                   files))
-                    }) %>%
-  unlist()
-epiexpert_forecasts <- purrr::map_dfr(files, readr::read_csv) %>%
-  dplyr::mutate(board_name = "EpiExpert-ensemble", 
-                submission_date = forecast_date,
-                horizon = as.numeric(gsub("([0-9]+).*$", "\\1", target))) %>%
-  dplyr::filter(grepl("inc", target), 
-                type == "quantile")
-
-data.table::fwrite(epiexpert_forecasts, 
-                   "human-forecasts/processed-forecast-data/all-epiexpert-forecasts.csv")
-
-
-
-
-# also read all EpiNow2 forecasts, give them a board_name 
-folders <- list.files("submissions/rt-forecasts/")
-files <- purrr::map(folders, 
-                    .f = function(folder_name) {
-                      files <- list.files(paste0("submissions/rt-forecasts/", 
-                                                 folder_name))
-                      paste(paste0("submissions/rt-forecasts/", 
-                                   folder_name, "/", 
-                                   files))
-                    }) %>%
-  unlist()
-epinow_forecasts <- purrr::map_dfr(files, readr::read_csv) %>%
-  dplyr::mutate(board_name = "EpiNow2", 
-                submission_date = forecast_date,
-                horizon = as.numeric(gsub("([0-9]+).*$", "\\1", target))) %>%
-  dplyr::filter(grepl("inc", target), 
-                type == "quantile")
-
-data.table::fwrite(epinow_forecasts, 
-                   "human-forecasts/processed-forecast-data/all-epinow2-forecasts.csv")
-
-# copy data into human forecast performance board app
-file.copy(from = here::here("human-forecasts", "processed-forecast-data"), 
-          to = here::here("human-forecasts", "performance-board"), recursive = TRUE)
-
-
-
-
-# also read all EpiNow2 secondary forecasts, give them a board_name 
-folders <- list.files("submissions/deaths-from-cases/")
-files <- purrr::map(folders, 
-                    .f = function(folder_name) {
-                      files <- list.files(paste0("submissions/deaths-from-cases/", 
-                                                 folder_name))
-                      paste(paste0("submissions/deaths-from-cases/", 
-                                   folder_name, "/", 
-                                   files))
-                    }) %>%
-  unlist()
-epinow_forecasts <- purrr::map_dfr(files, readr::read_csv) %>%
-  dplyr::mutate(board_name = "EpiNow2_secondary", 
-                submission_date = forecast_date,
-                horizon = as.numeric(gsub("([0-9]+).*$", "\\1", target))) %>%
-  dplyr::filter(grepl("inc", target), 
-                type == "quantile")
-
-data.table::fwrite(epinow_forecasts, 
-                   "human-forecasts/processed-forecast-data/all-epinow2_secondary-forecasts.csv")
-
-# copy data into human forecast performance board app
-file.copy(from = here::here("human-forecasts", "processed-forecast-data"), 
-          to = here::here("human-forecasts", "performance-board"), recursive = TRUE)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 
-# 
-# # empty google sheet
-# cols <- data.frame(matrix(ncol = ncol(forecasts), nrow = 0))
-# names(cols) <- names(forecasts)
-# googlesheets4::write_sheet(data = cols,
-#                            ss = spread_sheet,
-#                            sheet = "predictions")
 
