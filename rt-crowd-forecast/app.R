dc505ff42aa538f7d7723d0616b29754e27659e3
nikosbosse <nikosbosse@gmail.com>
2021-01-13 00:08:54 +0100

    update rt crowd forecast app and first test data

--- rt-crowd-forecast/app.R
+++ rt-crowd-forecast/app.R
@@ -7,25 +7,41 @@ library(magrittr)
 
 options( "golem.app.prod" = TRUE)
 
-# obs_filt_rt <- data.table::fread("../covid-german-forecasts/rt-forecast/data/summary/cases/2021-01-04/rt.csv") %>%
-#     dplyr::rename(value = median, 
+# Assume the submission date to be a Monday and the target end dates to be Saturdays
+submission_date <- "2021-01-11" 
+first_forecast_date <- as.character(as.Date("2021-01-11") - 16)
+
+
+
+# obs_filt_rt_cases <- data.table::fread("../covid-german-forecasts/rt-forecast/data/summary/cases/2021-01-11/rt.csv") %>%
+#     dplyr::rename(value = median,
 #                   target_end_date = date) %>%
-#     dplyr::filter(target_end_date < Sys.Date())
-# 
-# data.table::fwrite(obs_filt_rt, 
-#                    "./external-ressources/observations.csv")
+#   dplyr::mutate(target_type = "case",
+#                 target_end_date = as.Date(target_end_date)) %>%
+#     dplyr::filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
+#   dplyr::filter(region %in% c("Poland", "Germany"))
+# obs_filt_rt_deaths <- data.table::fread("../covid-german-forecasts/rt-forecast/data/summary/deaths/2021-01-11/rt.csv") %>%
+#   dplyr::rename(value = median,
+#                 target_end_date = date) %>%
+#   dplyr::mutate(target_type = "death",
+#                 target_end_date = as.Date(target_end_date)) %>%
+#   dplyr::filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
+#   dplyr::filter(region %in% c("Poland", "Germany"))
+# obs_filt_rt <- dplyr::bind_rows(obs_filt_rt_cases, obs_filt_rt_deaths) %>%
+#   dplyr::arrange(region, target_type, target_end_date)
+# data.table::fwrite(obs_filt_rt,
+#                    "rt-crowd-forecast/external-ressources/observations.csv")
 
 
 obs_filt_rt <- read.csv("external-ressources/observations.csv")
 
-submission_date <- "2021-01-11"
-first_forecast_date <- as.character(as.Date("2021-01-11") - 14)
+
 
 crowdforecastr::run_app(data = obs_filt_rt,
-                        selection_vars = c("region"), 
+                        selection_vars = c("region", "target_type"), 
                         first_forecast_date = first_forecast_date,
                         submission_date = submission_date,
-                        horizons = 6,
+                        horizons = 7,
                         horizon_interval = 7,
                         google_account_mail = "epiforecasts@gmail.com", 
                         forecast_sheet_id = "1g4OBCcDGHn_li01R8xbZ4PFNKQmV-SHSXFlv2Qv79Ks",
