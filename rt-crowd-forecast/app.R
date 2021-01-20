63be45e2ffc16933934764486701a47d2ba83cce
nikosbosse <nikosbosse@gmail.com>
2021-01-20 18:57:39 +0100

    update rt forecast

--- rt-crowd-forecast/app.R
+++ rt-crowd-forecast/app.R
@@ -1,6 +1,6 @@
 # Launch the ShinyApp (Do not remove this comment)
 # RT app
-
+# when on a new computer, update crowdforecastr
 # devtools::install_github("epiforecasts/crowdforecastr")
 
 library(crowdforecastr)
@@ -9,36 +9,35 @@ library(magrittr)
 options("golem.app.prod" = TRUE)
 
 # Assume the submission date to be a Monday and the target end dates to be Saturdays
-submission_date <- "2021-01-18" 
+submission_date <- "2021-01-25" 
 first_forecast_date <- as.character(as.Date(submission_date) - 16)
 
-# obs_filt_rt_cases <- data.table::fread(paste0("rt-forecast/data/summary/cases/", submission_date, "/rt.csv")) %>%
-#     dplyr::rename(value = median,
-#                   target_end_date = date) %>%
-#   dplyr::mutate(target_type = "case",
-#                 target_end_date = as.Date(target_end_date)) %>%
-#     dplyr::filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
-#   dplyr::filter(region %in% c("Poland", "Germany"))
-# obs_filt_rt_deaths <- data.table::fread(paste0("rt-forecast/data/summary/deaths/", submission_date, "/rt.csv")) %>%
-#   dplyr::rename(value = median,
-#                 target_end_date = date) %>%
-#   dplyr::mutate(target_type = "death",
-#                 target_end_date = as.Date(target_end_date)) %>%
-#   dplyr::filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
-#   dplyr::filter(region %in% c("Poland", "Germany"))
-# obs_filt_rt <- dplyr::bind_rows(obs_filt_rt_cases, obs_filt_rt_deaths) %>%
-#   dplyr::arrange(region, target_type, target_end_date)
-# data.table::fwrite(obs_filt_rt,
-#                    "rt-crowd-forecast/external-ressources/observations.csv")
-
-
-obs_filt_rt <- read.csv("external-ressources/observations.csv") %>%
-  dplyr::arrange(region, target_type, target_end_date)
-
-obs_filt_rt %>%
-  dplyr::filter(target_type == "case") %>%
-  dplyr::pull(target_end_date)
-
+# run on local machine to load the latest data. will be skipped on the siny server
+if (dir.exists("rt-forecast")) {
+  obs_filt_rt_cases <- data.table::fread(paste0("rt-forecast/data/summary/cases/", submission_date, "/rt.csv")) %>%
+      dplyr::rename(value = median,
+                    target_end_date = date) %>%
+    dplyr::mutate(target_type = "case",
+                  target_end_date = as.Date(target_end_date)) %>%
+      dplyr::filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
+    dplyr::filter(region %in% c("Poland", "Germany"))
+  obs_filt_rt_deaths <- data.table::fread(paste0("rt-forecast/data/summary/deaths/", submission_date, "/rt.csv")) %>%
+    dplyr::rename(value = median,
+                  target_end_date = date) %>%
+    dplyr::mutate(target_type = "death",
+                  target_end_date = as.Date(target_end_date)) %>%
+    dplyr::filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
+    dplyr::filter(region %in% c("Poland", "Germany"))
+  obs_filt_rt <- dplyr::bind_rows(obs_filt_rt_cases, obs_filt_rt_deaths) %>%
+    dplyr::arrange(region, target_type, target_end_date) %>%
+    dplyr::arrange(region, target_type, target_end_date)
+  
+  data.table::fwrite(obs_filt_rt,
+                     "rt-crowd-forecast/external-ressources/observations.csv")
+  
+} else {
+  obs_filt_rt <- read.csv("external-ressources/observations.csv") 
+}
 
 crowdforecastr::run_app(data = obs_filt_rt,
                         selection_vars = c("region", "target_type"), 
