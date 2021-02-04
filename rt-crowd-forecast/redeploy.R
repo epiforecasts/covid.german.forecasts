8b1f1d27f8aa3811f8774074a6391c37dff4c748
Sam Abbott <s.e.abbott12@gmail.com>
2021-02-04 16:53:02 +0000

    update redeploy

--- rt-crowd-forecast/redeploy.R
+++ rt-crowd-forecast/redeploy.R
@@ -9,34 +9,22 @@ if (weekdays(Sys.Date()) != "Monday") {
 } else {
   submission_date <- Sys.Date()
 }
-saveRDS(submission_date, 
+saveRDS(submission_date,
         here("rt-crowd-forecast", "data", "submission_date.RDS"))
 first_forecast_date <- as.character(as.Date(submission_date) - 16)
 
 # copy Rt data into app
-obs_filt_rt_cases <-
+obs <-
   fread(
     here("rt-forecast", "data", "summary", "cases", submission_date, "rt.csv")
     ) %>%
   rename(value = median, target_end_date = date) %>%
   mutate(target_type = "case", target_end_date = as.Date(target_end_date)) %>%
   filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
-  filter(region %in% c("Poland", "Germany"))
-
-obs_filt_rt_deaths <-
-  fread(
-    here("rt-forecast", "data", "summary", "deaths", submission_date, "rt.csv")
-    ) %>%
-  rename(value = median, target_end_date = date) %>%
-  mutate(target_type = "death", target_end_date = as.Date(target_end_date)) %>%
-  filter(target_end_date <= (as.Date(first_forecast_date) + 7 * 6)) %>%
-  filter(region %in% c("Poland", "Germany"))
-
-obs_filt_rt <- bind_rows(obs_filt_rt_cases, obs_filt_rt_deaths) %>%
-  arrange(region, target_type, target_end_date) %>%
+  filter(region %in% c("Poland", "Germany")) %>%
   arrange(region, target_type, target_end_date)
 
-fwrite(obs_filt_rt, here("rt-crowd-forecast", "data", "observations.csv"))
+fwrite(obs, here("rt-crowd-forecast", "data", "observations.csv"))
 
 deployApp(
   appDir = here("rt-crowd-forecast"),
