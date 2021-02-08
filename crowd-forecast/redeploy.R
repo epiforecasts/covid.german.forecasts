24b75f2cca5ce85dce76fea8e7c88ef140bf2095
Sam Abbott <s.e.abbott12@gmail.com>
2021-02-08 10:08:20 +0000

    reformat

--- crowd-forecast/redeploy.R
+++ crowd-forecast/redeploy.R
@@ -6,7 +6,8 @@ library(rsconnect)
 source(here("data-raw", "update.R"))
 
 # copy data into app
-file.copy(c(here("data-raw", "weekly-incident-cases.csv"), here("data-raw", "weekly-incident-deaths.csv")),
+file.copy(c(here("data-raw", "weekly-incident-cases.csv"), 
+            here("data-raw", "weekly-incident-deaths.csv")),
           to = here("crowd-forecast", "data-raw"), overwrite = TRUE)
 
 # if today is not Monday, set submission date to the next Monday
@@ -16,7 +17,9 @@ if (weekdays(Sys.Date()) != "Monday") {
   submission_date <- Sys.Date()
 }
 
-saveRDS(submission_date, here("crowd-forecast", "data-raw", "submission_date.rds"))
+saveRDS(
+  submission_date, here("crowd-forecast", "data-raw", "submission_date.rds")
+  )
 
 deployApp(appDir = here("crowd-forecast"),
           appName = "crowd-forecast",
