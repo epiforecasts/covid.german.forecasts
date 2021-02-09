e7df9047261a205a5ff93d850a78291363c86203
Sam Abbott <s.e.abbott12@gmail.com>
2021-02-09 18:44:26 +0000

    clean up report.R

--- evaluation/report.R
+++ evaluation/report.R
@@ -3,7 +3,7 @@ library(purrr)
 library(dplyr)
 library(here)
 library(readr)
-library(scoringutils) 
+library(scoringutils)
 library(data.table)
 
 devtools::install_github("epiforecasts/scoringutils", ref = "dev")
@@ -12,75 +12,86 @@ devtools::install_github("epiforecasts/scoringutils", ref = "dev")
 # to one file and copy them into the crowd forecast app folder 
 # having them in one place allows to easily include other models in the 
 # crowd forecast report. Could in principle also do without copying
-load_and_copy_forecasts <- function(root_dir, 
-                                    out_file_path, 
+load_and_copy_forecasts <- function(root_dir,
+                                    out_file_path,
                                     new_board_name) {
   folders <- list.files(root_dir)
-  files <- purrr::map(folders, 
-                      .f = function(folder_name) {
-                        files <- list.files(here::here(root_dir, folder_name))
-                        paste(here::here(root_dir, folder_name, files))
-                      }) %>%
+  files <- map(folders,
+  .f = function(folder_name) {
+    files <- list.files(here(root_dir, folder_name))
+    paste(here(root_dir, folder_name, files))
+    }) %>%
     unlist()
-  
-  forecasts <- purrr::map_dfr(files, readr::read_csv) %>%
-    dplyr::mutate(board_name = new_board_name, 
-                  submission_date = forecast_date,
-                  horizon = as.numeric(gsub("([0-9]+).*$", "\\1", target))) %>%
-    dplyr::filter(grepl("inc", target), 
-                  type == "quantile")
-  
-  data.table::fwrite(forecasts, out_file_path)
+
+  forecasts <- suppressMessages(map_dfr(files, read_csv) %>%
+    mutate(board_name = new_board_name, 
+           submission_date = forecast_date,
+           horizon = as.numeric(gsub("([0-9]+).*$", "\\1", target))) %>%
+    filter(grepl("inc", target),
+                  type == "quantile"))
+  fwrite(forecasts, out_file_path)
 }
 
 # read in the EpiExpert ensemble forecast and EpiNow2 models
-load_and_copy_forecasts(root_dir = here::here("submissions", "crowd-forecasts"), 
-                        out_file_path = here::here("crowd-forecast", "processed-forecast-data", "all-epiexpert-forecasts.csv"), 
-                        new_board_name = "EpiExpert-ensemble")
+load_and_copy_forecasts(
+  root_dir = here("submissions", "crowd-forecasts"), 
+  out_file_path = here("crowd-forecast", "processed-forecast-data",
+                       "all-epiexpert-forecasts.csv"), 
+  new_board_name = "EpiExpert-ensemble"
+  )
 
 # also read all EpiNow2 forecasts, give them a board_name 
-load_and_copy_forecasts(root_dir = here::here("submissions", "rt-forecasts"), 
-                        out_file_path = here::here("crowd-forecast", "processed-forecast-data", "all-epinow2-forecasts.csv"), 
-                        new_board_name = "EpiNow2")
+load_and_copy_forecasts(
+  root_dir = here("submissions", "rt-forecasts"), 
+  out_file_path = here("crowd-forecast", "processed-forecast-data", 
+                       "all-epinow2-forecasts.csv"), 
+  new_board_name = "EpiNow2"
+  )
 
 # also read all EpiNow2 secondary forecasts, give them a board_name 
-load_and_copy_forecasts(root_dir = here::here("submissions", "deaths-from-cases"), 
-                        out_file_path = here::here("crowd-forecast", "processed-forecast-data", "all-epinow2_secondary-forecasts.csv"), 
-                        new_board_name = "EpiNow2_secondary")
+load_and_copy_forecasts(
+  root_dir = here("submissions", "deaths-from-cases"), 
+  out_file_path = here("crowd-forecast", "processed-forecast-data", 
+                       "all-epinow2_secondary-forecasts.csv"), 
+  new_board_name = "EpiNow2_secondary"
+  )
 
 # also read all EpiNow2 Rt crowd forecasts, give them a board_name 
-load_and_copy_forecasts(root_dir = here::here("submissions", "crowd-rt-forecasts"), 
-                        out_file_path = here::here("crowd-forecast", "processed-forecast-data", "all-crowd-rt-forecasts.csv"), 
-                        new_board_name = "Crowd-Rt-Forecast")
+load_and_copy_forecasts(
+  root_dir = here("submissions", "crowd-rt-forecasts"), 
+  out_file_path = here("crowd-forecast", "processed-forecast-data", 
+                       "all-crowd-rt-forecasts.csv"), 
+  new_board_name = "Crowd-Rt-Forecast")
 
 
 # load all data ----------------------------------------------------------------
-root_dir <- here::here("crowd-forecast", "processed-forecast-data")
-file_paths_forecast <- here::here(root_dir, list.files(root_dir))
+root_dir <- here("crowd-forecast", "processed-forecast-data")
+file_paths_forecast <- here(root_dir, list.files(root_dir))
 
-prediction_data <- purrr::map_dfr(file_paths_forecast, 
-                                  .f = function(x) {
-                                    data <- data.table::fread(x) %>%
-                                      dplyr::mutate(target_end_date = as.Date(target_end_date), 
-                                                    submission_date = as.Date(submission_date), 
-                                                    forecast_date = as.Date(forecast_date))
-                                  }) %>%
-  dplyr::mutate(target_type = ifelse(grepl("death", target), "death", "case")) %>%
-  dplyr::rename(prediction = value) %>%
-  dplyr::mutate(forecast_date = as.Date(submission_date)) %>%
-  dplyr::rename(model = board_name) %>%
-  dplyr::filter(type == "quantile", 
+prediction_data <- map_dfr(file_paths_forecast, 
+.f = function(x) {
+  data <- fread(x) %>%
+  mutate(target_end_date = as.Date(target_end_date), 
+  submission_date = as.Date(submission_date),
+  forecast_date = as.Date(forecast_date))
+  }) %>%
+  mutate(target_type = ifelse(grepl("death", target), "death", "case")) %>%
+  rename(prediction = value) %>%
+  mutate(forecast_date = as.Date(submission_date)) %>%
+  rename(model = board_name) %>%
+  filter(type == "quantile",
                 location_name %in% c("Germany", "Poland")) %>%
-  dplyr::select(location, location_name, forecast_date, quantile, prediction, model, target_end_date, horizon, target, target_type)
+  select(location, location_name, forecast_date, quantile, prediction,
+         model, target_end_date, horizon, target, target_type)
 
 files <- list.files("data-raw/")
 file_paths <- paste0("data-raw/", files[grepl("weekly-incident", files)])
 names(file_paths) <- c("case", "death")
 
-truth_data <- purrr::map_dfr(file_paths, readr::read_csv, .id = "target_type") %>%
-  dplyr::rename(true_value = value) %>%
-  dplyr::mutate(target_end_date = as.Date(target_end_date)) %>%
-  dplyr::arrange(location, target_type, target_end_date)
+truth_data <- map_dfr(file_paths, read_csv, .id = "target_type") %>%
+  rename(true_value = value) %>%
+  mutate(target_end_date = as.Date(target_end_date)) %>%
+  arrange(location, target_type, target_end_date)
 
 intro_text <- c("This our own evaluation of individual forecasts submitted to the 
 EpiExpert model. This model is submitted to the 
@@ -99,13 +110,11 @@ params <- list(locations = c("Germany", "Poland"),
                target_types = "all",
                intro_text = intro_text)
 
-scoringutils::render_scoring_report(truth_data = truth_data, 
-                                    document_title = "EpiExpert Crowd-Forecasting Performance Board",
-                                    params = params,
-                                    prediction_data = prediction_data, 
-                                    save_dir = "docs/", 
-                                    filename = "index.html")
-
-
-
-
+render_scoring_report(
+  truth_data = truth_data,
+  document_title = "EpiExpert Crowd-Forecasting Performance Board",
+  params = params,
+  prediction_data = prediction_data,
+  save_dir = "docs/",
+  filename = "index.html"
+  )
