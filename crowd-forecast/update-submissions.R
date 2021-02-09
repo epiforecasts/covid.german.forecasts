6b33c55c04dc3d187be427626954f26335c63889
Sam Abbott <s.e.abbott12@gmail.com>
2021-02-09 18:58:52 +0000

    format code

--- crowd-forecast/update-submissions.R
+++ crowd-forecast/update-submissions.R
@@ -62,10 +62,12 @@ replace_date_and_time <- function(forecasts) {
     group_by(forecaster_id) %>%
     dmutate(forecast_duration = c(NA, diff(forecast_time))) %>%
     ungroup()
-  
-  forecasts <- inner_join(forecasts, forecast_times, 
-                          by = c("forecaster_id", "location",  "target_type", "forecast_time")) %>%
-    mutate(forecast_week = epiweek(forecast_date), target_end_date = as.Date(target_end_date)) %>%
+
+  forecasts <- inner_join(
+    forecasts, forecast_times,
+    by = c("forecaster_id", "location", "target_type", "forecast_time")) %>%
+    mutate(forecast_week = epiweek(forecast_date),
+           target_end_date = as.Date(target_end_date)) %>%
     select(-forecast_time)
   return(forecasts)
 }
@@ -76,25 +78,34 @@ filtered_forecasts <- replace_date_and_time(filtered_forecasts)
 
 # write raw forecasts
 fwrite(raw_forecasts %>% select(-board_name),
-       here("crowd-forecast", "raw-forecast-data", submission_date, "-raw-forecasts.csv"))
+       here("crowd-forecast", "raw-forecast-data", 
+            submission_date, "-raw-forecasts.csv"))
 
 # obtain quantiles from forecasts ----------------------------------------------
 # define function that returns quantiles depending on condition and distribution
-calculate_quantiles <- function(quantile_grid, median, width, forecast_type, distribution, lower_90, upper_90) {
+calculate_quantiles <- function(quantile_grid, median, width, forecast_type, 
+                                distribution, lower_90, upper_90) {
   
   if (distribution == "log-normal") {
-    values <- list(exp(qnorm(quantile_grid, mean = log(as.numeric(median)), sd = as.numeric(width))))
+    values <- list(exp(qnorm(
+      quantile_grid, mean = log(as.numeric(median)), sd = as.numeric(width)
+      )))
   } else if (distribution == "normal") {
-    values <- list((qnorm(quantile_grid, mean = (as.numeric(median)), sd = as.numeric(width))))
-    
+    values <- list((qnorm(
+      quantile_grid, mean = (as.numeric(median)), sd = as.numeric(width)
+      )))
   } else if (distribution == "cubic-normal") {
-    values <- list((qnorm(quantile_grid, mean = (as.numeric(median) ^ (1/3)), sd = as.numeric(width))) ^ 3)
-    
+    values <- list((qnorm(quantile_grid,
+     mean = (as.numeric(median) ^ (1 / 3)), sd = as.numeric(width))
+      ) ^ 3)
   } else if (distribution == "fifth-power-normal") {
-    values <- list((qnorm(quantile_grid, mean = (as.numeric(median) ^ (1/5)), sd = as.numeric(width))) ^ 5)
-    
+    values <- list((qnorm(quantile_grid, 
+    mean = (as.numeric(median) ^ (1 / 5)), sd = as.numeric(width)
+      )) ^ 5)
   } else if (distribution == "seventh-power-normal") {
-    values <- list((qnorm(quantile_grid, mean = (as.numeric(median) ^ (1/7)), sd = as.numeric(width))) ^ 7)
+    values <- list((qnorm(quantile_grid,
+     mean = (as.numeric(median) ^ (1 / 7)), sd = as.numeric(width))
+     ) ^ 7)
   }
   return(values)
 }
@@ -103,15 +114,18 @@ forecast_quantiles <- filtered_forecasts %>%
   # disregard quantile forecasts this week
   rowwise() %>%
   mutate(quantile = list(quantile_grid),
-        value = calculate_quantiles(quantile_grid, median, width, forecast_type, distribution, lower_90, upper_90)) %>%
+        value = calculate_quantiles(quantile_grid, median, width, 
+        forecast_type, distribution, lower_90, upper_90)) %>%
   unnest(cols = c(quantile, value)) %>%
   ungroup() %>%
-  mutate(type = ifelse(target_type == "cases", "case", "death"), target = paste0(horizon, " wk ahead inc ", type), 
+  mutate(type = ifelse(target_type == "cases", "case", "death"), 
+         target = paste0(horizon, " wk ahead inc ", type), 
          type = "quantile")
 
 # save forecasts in quantile-format
 fwrite(forecast_quantiles %>% mutate(submission_date = submission_date),
-       here("crowd-forecast", "processed-forecast-data", submission_date, "-processed-forecasts.csv"))
+       here("crowd-forecast", "processed-forecast-data", 
+       submission_date, "-processed-forecasts.csv"))
 
 # omit forecasters who haven't forecasted at least two targets
 forecasters_to_omit <- forecast_quantiles %>%
@@ -127,21 +141,25 @@ forecast_quantiles <- forecast_quantiles %>%
   filter(!(forecaster_id %in% forecasters_to_omit))
   
 if (median_ensemble) {
-  # make median ensemble ---------------------------------------------------------
+  # make median ensemble
   forecast_inc <- forecast_quantiles %>%
     mutate(target_end_date = as.Date(target_end_date)) %>%
-    group_by(location, location_name, target, type, quantile, horizon, target_end_date) %>%
+    group_by(location, location_name, target, type,
+     quantile, horizon, target_end_date) %>%
     summarise(value = median(value)) %>%
     ungroup() %>%
-    select(target, target_end_date, location, type, quantile, value, location_name)
+    select(target, target_end_date, location, type,
+     quantile, value, location_name)
 } else {
-  # make mean ensemble ---------------------------------------------------------
+  # make mean ensemble
   forecast_inc <- forecast_quantiles %>%
     mutate(target_end_date = as.Date(target_end_date)) %>%
-    group_by(location, location_name, target, type, quantile, horizon, target_end_date) %>%
+    group_by(location, location_name, target, type,
+     quantile, horizon, target_end_date) %>%
     summarise(value = mean(value)) %>%
     ungroup() %>%
-    select(target, target_end_date, location, type, quantile, value, location_name)
+    select(target, target_end_date, location, 
+    type, quantile, value, location_name)
 }
 # add point forecast
   forecast_inc <- bind_rows(forecast_inc, 
@@ -158,14 +176,18 @@ first_forecast_date <- forecasts %>%
   unique() %>%
   min(na.rm = TRUE)
 
-deaths <- get_truth_data(dir = here("data-raw"), range = "weekly", type = "cumulative",
-                         target = "deaths", locs = c("GM", "PL")) %>%
+deaths <-
+get_truth_data(dir = here("data-raw"), range = "weekly",
+               type = "cumulative", target = "deaths", 
+               locs = c("GM", "PL")) %>%
   group_by(location) %>%
   filter(target_end_date == as.Date(first_forecast_date - 7)) %>%
   rename(case = type)
 
-cases <- get_truth_data(dir = here("data-raw"), range = "weekly", type = "cumulative",
-                         target = "cases", locs = c("GM", "PL")) %>%
+cases <- 
+get_truth_data(dir = here("data-raw"), range = "weekly",
+               type = "cumulative", target = "cases", 
+               locs = c("GM", "PL")) %>%
   group_by(location) %>%
   filter(target_end_date == as.Date(first_forecast_date - 7)) %>%
   rename(case = type)
@@ -178,7 +200,7 @@ last_obs <- bind_rows(deaths, cases) %>%
 forecast_cum <- forecast_inc %>%
   mutate(case = ifelse(grepl("case", target), "case", "death")) %>%
   group_by(location, quantile, case) %>%
-  mutate(value = cumsum(value), 
+  mutate(value = cumsum(value),
                 target = gsub("inc", "cum", target)) %>%
   ungroup() %>%
   # add last observed value
@@ -195,19 +217,23 @@ check_dir(here("submissions", "crowd-forecast", submission_date))
 forecast_submission %>%
   filter(location_name %in% "Germany", 
                 grepl("death", target)) %>%
-  fwrite(here("submissions", "crowd-forecast", submission_date, paste0(submission_date, "-Germany-epiforecasts-EpiExpert.csv")))
+  fwrite(here("submissions", "crowd-forecast", submission_date, 
+         paste0(submission_date, "-Germany-epiforecasts-EpiExpert.csv")))
 
 forecast_submission %>%
   filter(location_name %in% "Germany", 
                 grepl("case", target)) %>%
-  fwrite(here("submissions", "crowd-forecast", submission_date, paste0(submission_date, "-Germany-epiforecasts-EpiExpert-case.csv")))
+  fwrite(here("submissions", "crowd-forecast", submission_date, 
+         paste0(submission_date, "-Germany-epiforecasts-EpiExpert-case.csv")))
 
 forecast_submission %>%
   filter(location_name %in% "Poland", 
                 grepl("death", target)) %>%
-  fwrite(here("submissions", "crowd-forecast", submission_date, paste0(submission_date, "-Poland-epiforecasts-EpiExpert.csv")))
+  fwrite(here("submissions", "crowd-forecast", submission_date, 
+         paste0(submission_date, "-Poland-epiforecasts-EpiExpert.csv")))
 
 forecast_submission %>%
   filter(location_name %in% "Poland", 
                 grepl("case", target)) %>%
-  fwrite(here("submissions", "crowd-forecast", submission_date, paste0(submission_date, "-Poland-epiforecasts-EpiExpert-case.csv")))
+  fwrite(here("submissions", "crowd-forecast", submission_date, 
+         paste0(submission_date, "-Poland-epiforecasts-EpiExpert-case.csv")))
