c9916dea6aa73c34e40232d6060995bfed8d64ec
nikosbosse <nikosbosse@gmail.com>
2021-03-09 15:54:31 +0100

    don't redeploy as this is handled by the ECDC repo

--- crowd-forecast/update.sh
+++ crowd-forecast/update.sh
@@ -2,6 +2,3 @@
 
 # create crowd forecast app submission data
 Rscript crowd-forecast/update-submissions.R
-
-# Redeploy forecast app (to update submission date to next week)
-Rscript crowd-forecast/redeploy.R
