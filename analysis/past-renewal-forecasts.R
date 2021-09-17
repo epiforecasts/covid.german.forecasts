eb7a39e0a6a203f0be0eee8d3793f9fa7568a3ea
nikosbosse <nikosbosse@gmail.com>
2021-09-17 18:54:51 +0200

    add files to run past renewal and convolution forecasts

---
+++ analysis/past-renewal-forecasts.R
@@ -0,0 +1,60 @@
+# Packages -----------------------------------------------------------------
+library(covid.german.forecasts)
+library(EpiNow2, quietly = TRUE)
+library(data.table, quietly = TRUE)
+library(future, quietly = TRUE)
+library(here, quietly = TRUE)
+library(lubridate, quietly = TRUE)
+
+# Set target dates ---------------------------------------------------------
+dates <- as.character(as.Date("2020-10-12") + 7*(0:9))
+
+# Update delays -----------------------------------------------------------
+generation_time <- readRDS(here("rt-forecast", "data", "delays", "generation_time.rds"))
+incubation_period <- readRDS(here("rt-forecast", "data" ,"delays", "incubation_period.rds"))
+onset_to_report <- readRDS(here("rt-forecast", "data", "delays", "onset_to_report.rds"))
+
+
+for (target_date in dates) {
+  # Get cases  ---------------------------------------------------------------
+  cases <- fread(file.path("data-raw", "daily-incidence-cases.csv"))
+  cases <- cases[, .(region = as.character(location_name), date = as.Date(date), confirm = value)]
+  cases <- cases[confirm < 0, confirm := 0]
+  cases <- cases[date >= (max(date) - weeks(12))]
+  setorder(cases, region, date)
+  
+  # Set up parallel execution -----------------------------------------------
+  no_cores <- setup_future(cases)
+  
+  # Run Rt estimation -------------------------------------------------------
+  rt <- opts_list(rt_opts(prior = list(mean = 1.1, sd = 0.2), future = "latest"), cases)
+  # add population adjustment for each country
+  loc_names <- names(rt)
+  rt <- lapply(loc_names,  function(loc) {
+    rt_loc <- rt[[loc]]
+    rt_loc$pop <- locations[location_name %in% loc, ]$population
+    return(rt_loc)
+  })
+  names(rt) <- loc_names
+  
+  regional_epinow(reported_cases = cases,
+                  generation_time = generation_time, 
+                  delays = delay_opts(incubation_period, onset_to_report),
+                  rt = rt,
+                  stan = stan_opts(samples = 2000, warmup = 250, 
+                                   chains = 4, cores = no_cores),
+                  obs = obs_opts(scale = list(mean = 0.25, sd = 0.05)),
+                  horizon = 30,
+                  output = c("region", "summary", "timing", "samples", "fit"),
+                  target_date = target_date,
+                  target_folder = here("rt-forecast-retrospective", "data", "samples", "cases"), 
+                  summary_args = list(summary_dir = here("rt-forecast-retrospective", 
+                                                         "data", "summary", 
+                                                         "cases", target_date),
+                                      all_regions = TRUE),
+                  logs = "rt-forecast-retrospective/logs/cases", verbose = TRUE)
+  
+  plan("sequential")
+}
+
+
