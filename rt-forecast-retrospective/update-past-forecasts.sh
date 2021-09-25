34fa4f3732079c027b98bad5ae7270764829dc28
nikosbosse <nikosbosse@gmail.com>
2021-09-25 15:48:34 +0200

    update script to rerun past forecasts

--- rt-forecast-retrospective/update-past-forecasts.sh
+++ rt-forecast-retrospective/update-past-forecasts.sh
@@ -1,4 +1,9 @@
 #!bin/bash
+
+Rscript rt-forecast-retrospective/past-renewal-forecasts.R
+Rscript rt-forecast-retrospective/past-convolution-forecasts.R
+Rscript rt-forecast-retrospective/past-submissions.R
+
 cd submissions
 cd rt-forecasts-retrospective
 find . -name '*.csv' -exec cp {} ../../data-raw/epiforecasts-EpiNow2-retrospective/ \;
