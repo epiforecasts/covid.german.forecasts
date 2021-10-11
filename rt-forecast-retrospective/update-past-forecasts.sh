fc09d7bc2a938692348ccbdc9742cae82128d659
nikosbosse <nikosbosse@gmail.com>
2021-10-11 13:46:31 +0200

    set up trackdown manuscript

--- rt-forecast-retrospective/update-past-forecasts.sh
+++ rt-forecast-retrospective/update-past-forecasts.sh
@@ -4,14 +4,5 @@ Rscript rt-forecast-retrospective/past-renewal-forecasts.R
 Rscript rt-forecast-retrospective/past-convolution-forecasts.R
 Rscript rt-forecast-retrospective/past-submissions.R
 
-cd submissions
-cd rt-forecasts-retrospective
-find . -name '*.csv' -exec cp {} ../../data-raw/epiforecasts-EpiNow2-retrospective/ \;
-cd ..
-
-cd deaths-from-cases-retrospective
-find . -name '*.csv' -exec cp {} ../../data-raw/epiforecasts-EpiNow2_secondary-retrospective/ \;
-cd ../..
-
   
   
