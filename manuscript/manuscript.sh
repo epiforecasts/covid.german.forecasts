f73da16ae44cc1cbc2de451ca2c1c30a8a47d5a0
nikosbosse <nikosbosse@gmail.com>
2021-10-14 18:18:09 +0200

    clean code

--- manuscript/manuscript.sh
+++ manuscript/manuscript.sh
@@ -2,3 +2,5 @@
 
 Rscript -e 'trackdown::update_file(file = "manuscript/manuscript.Rmd", gfile = "Comparing human and model-based forecasts of COVID-19 in Germany and Poland", hide_code = TRUE)'
 
+
+trackdown::download_file(file = "manuscript/manuscript.Rmd", gfile = "Comparing human and model-based forecasts of COVID-19 in Germany and Poland")
